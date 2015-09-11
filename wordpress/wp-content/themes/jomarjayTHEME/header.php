46209adc660dd4cac0aa8e62b137d4b479129bd1
Jomar Jay B. Corona <trd.jomarjaycorona@gmail.com>
2015-09-11 12:25:46 +0800

    adding custom body class, post loop ->

--- wordpress/wp-content/themes/jomarjayTHEME/header.php
+++ wordpress/wp-content/themes/jomarjayTHEME/header.php
@@ -6,7 +6,21 @@
 	<title>Jomar Jay</title>
 	<?php wp_head(); ?>
 </head>
-<body>
+
+<?php 
+	//is_home() -> means asa tanan post nimo gibutang
+	//is_front_page()  -> ug asa imong imong HOME PAGE
+
+	if(is_front_page() ):
+		$jomarjay_classes = array( 'jomarjay-class','my-class');
+	else:
+		$jomarjay_classes = array( 'no-jomarjay-class');
+	endif;
+
+
+ ?>
+
+<body <?php body_class($jomarjay_classes);		//body_class(array('jomarjay-class','my-class')); ?>>
 
 
 <?php  wp_nav_menu(array('theme_location'=>'primary')); ?>
